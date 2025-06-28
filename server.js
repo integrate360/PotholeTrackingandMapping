e9576292502dd37b187f3fbaee38af81753b28d2
integrate360 <integrate360@gmail.com>
2025-06-28 17:08:57 +0530

    add the getAllImages

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import app from './app.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-const PORT = process.env.PORT || 8000;
+
 dotenv.config();
+const PORT = process.env.PORT || 8000;
 
 // Connect to MongoDB
 connectDB();
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
